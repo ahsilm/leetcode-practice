c8ecb9ca0b421bdfa2c429eca5a5601f08a96b6d
ahsilm <43988929+ahsilm@users.noreply.github.com>
2025-08-07 23:12:01 -0400

    Time: 8 ms (40.24%) | Memory: 21.3 MB (90.20%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,30 +2,36 @@ class MinStack:
 
     def __init__(self):
         self.stack= []
+        self.minStack = []
     
-    def push(self, val: int) -> None:
-        self.stack.append(val)    
-
+    def push(self, val: int) -> None: 
+        self.stack.append(val)   
+        if(len(self.minStack) == 0):
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val, self.minStack[-1]))
     def pop(self) -> None:
         self.stack.pop(-1)
+        self.minStack.pop(-1)
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        lastidx = len(self.stack) - 1
-        minEl = self.stack[-1]
-        arr = [lastidx]
-        for i in range(0,len(self.stack)-1):
-            currEl = self.stack[i]
-            if currEl < minEl:
-                minEl = currEl
-                arr.append(i)
-            else:
-                continue
+        return self.minStack[-1]
+        # lastidx = len(self.stack) - 1
+        # minEl = self.stack[-1]
+        # arr = [lastidx]
+        # for i in range(0,len(self.stack)-1):
+        #     currEl = self.stack[i]
+        #     if currEl < minEl:
+        #         minEl = currEl
+        #         arr.append(i)
+        #     else:
+        #         continue
 
-        topidx = arr[-1]   
-        return(self.stack[topidx])
+        # topidx = arr[-1]   
+        # return(self.stack[topidx])
                 
 
 
