3f6d49c007370a5782ea80b167baf425a7562d39
ahsilm <43988929+ahsilm@users.noreply.github.com>
2025-08-12 01:17:36 -0400

    Time: 35 ms (83.49%) | Memory: 27 MB (8.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -12,37 +12,5 @@ class Solution:
             if profit < diff:
                 profit = diff
         return profit
-        # profit = 0
-        # for i in range(len(prices)-1):
-        #     maxVal = 0
-        #     for j in range(i+1, len(prices)):
-        #         if prices[j] > maxVal:
-        #             maxVal = prices[j]
-        #     diff = maxVal - prices[i]
-        #     if diff > profit:
-        #         profit = diff
-
-        return profit
-        # profit = 0
-        # for i in range(len(prices)-1):
-        #     maxVal = max(prices[i+1:len(prices)])
-        #     diff = maxVal - prices[i] 
-        #     if diff > profit:
-        #         profit = diff
-        # return profit 
-        # i = 0
-        # j = 1
-        # diff = 0
-        # while( (i < len(prices)-1) and (i < j)):
-        #     val = prices[j] - prices[i]
-        #     if val > diff:
-        #             diff = val
-        #     j += 1
-        #     if (j == len(prices)):
-        #         i += 1
-        #         j = i+1
-
-        # return diff
-
-        
+      
         
